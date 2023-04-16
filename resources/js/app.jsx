40728eb3bd76feb908440824f012d09884bfcd07
minwoo <sasyo1z2@naver.com>
2023-04-17 00:23:41 +0900

    React-Bootstrap 적용

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,17 @@
 import "./bootstrap";
 
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from "react-bootstrap";
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <button>Simple Button</button>
+            <Button>Bootstrap</Button>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
