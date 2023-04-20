2c600bf0d0388a73aaac811dbed64c86b9820043
minwoo <sasyo1z2@naver.com>
2023-04-21 02:09:47 +0900

    파비콘 추가, 타이틀 변경

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('assets/img/favicon.png') }}" sizes="128x128" rel="shortcut icon"/>
 
-    <title>test project</title>
+    <title>MinWoo`s Todo List</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
