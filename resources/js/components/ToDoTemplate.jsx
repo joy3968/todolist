5af3d2547b76bc83d2a3cc03949fabd59d24a3cf
minwoo <sasyo1z2@naver.com>
2023-04-21 01:27:45 +0900

    CSS 변경 - 템플릿 반응형 처리.

--- resources/js/components/ToDoTemplate.jsx
+++ resources/js/components/ToDoTemplate.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TodoTemplateBlock = styled.div`
+  /* 모바일 처리 */
+  @media (max-width: 768px) {
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 10px;
+  }
   width: 412px;
-  height: 768px;
+  height: 700px;
 
   position: relative;
   background: white;
@@ -11,8 +17,7 @@ const TodoTemplateBlock = styled.div`
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
 
   margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
-
-  margin-top: 96px;
+  margin-top: 30px;
   margin-bottom: 32px;
   display: flex;
   flex-direction: column;
